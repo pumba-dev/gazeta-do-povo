6530d82b1e7e237b52714eb27ebf11d3aaf24f8e
pumba-dev <dudu32769144@gmail.com>
2022-01-18 11:38:35 -0300

    Config Web Component

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-import './styles/global.css'
+import './styles/reset.css'
 import './styles/style.css'
 import './styles/colors.css'
 
-const $root = document.querySelector('#app')
-$root.insertAdjacentHTML("beforeend", `
-  <h1>Front End Test!</h1>
-  <h2>Clube Gazeta do Povo</h2>
-`);
+class $RootApp extends HTMLElement {
+  constructor() {
+    super()
+    this.build()
+  }
+
+  build() {
+    const shadow = this.attachShadow({ mode: 'open' })
+
+    const $title = document.createElement('h1')
+    $title.classList.add('root-app')
+    $title.textContent = "Pumba Developer"
+
+    shadow.appendChild($title)
+    shadow.appendChild(this.styles())
+  }
+
+  styles() {
+    const style = document.createElement('style')
+    style.textContent = `
+      .root-app {
+        text-align: center;
+      }
+    `
+    return style
+  }
+}
+
+customElements.define('root-app', $RootApp)
 
 
